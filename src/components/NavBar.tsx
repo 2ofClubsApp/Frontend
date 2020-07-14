91de9fd303c85245a7fe63c7f837efec124adbea
hillarytang <hillary.s.tang@gmail.com>
2020-07-14 14:38:23 -0400

    Navbar with dropdown added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
-import {Button, Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Button, Navbar, Nav, NavDropdown, Dropdown, NavLink} from "react-bootstrap";
 import React from "react";
 import {useHistory} from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import "./NavBar.css"
 
 library.add(faBars)
 
@@ -16,24 +17,29 @@ const NavBar = () => {
 
     return (
         <>
-        <Navbar collapseOnSelect expand="lg">
+        <Navbar collapseOnSelect expand="lg" className={"bluebg d-flex justify-content-end"}>
             <Button variant="outline-light" className="m-2 text-uppercase" onClick={() => changeRoute('/')}>2OFCLUBS
             </Button>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto">
-                
-                <NavDropdown title={<FontAwesomeIcon icon={faBars}/>} id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+            <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                    <Dropdown alignRight>
+                    <Dropdown.Toggle id="user-dropdown" as={NavLink}>
+                        <FontAwesomeIcon icon={faBars}/>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+
+                        <Dropdown.Item href="/">Create a club</Dropdown.Item>
+                        <Dropdown.Item href="/">Manage your clubs</Dropdown.Item>
+                        <Dropdown.Item href="/">Your settings</Dropdown.Item>
+                        <Dropdown.Item href="/">Log out</Dropdown.Item>
+                    </Dropdown.Menu>
+                    </Dropdown>
                 </Nav>
-            </Navbar.Collapse>
+                </Navbar.Collapse>
         </Navbar>
-  </>
+        </>
     )
 }
 export default NavBar
