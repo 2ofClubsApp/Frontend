55a29090f195ecb17e905636e314534333ab5109
hillarytang <hillary.s.tang@gmail.com>
2020-08-23 23:17:45 -0400

    #18 Club managers can be added/removed by other managers

--- src/components/Club/ClubAdvancedSettings/ClubManagerListing.tsx
+++ src/components/Club/ClubAdvancedSettings/ClubManagerListing.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { StatusResponse } from "../../../types/DataResponses";
+import axios from "../../../axios";
+import { Button } from "react-bootstrap";
 
 library.add(faTrashAlt)
 
 type ClubManagerListingDefinition = {
-    title: string
-    active: boolean
+    clubID: number
+    username: string
+    userToken: string
+    setManagerUsername: any
+    confirmPopup: any
 }
 
-const ClubManagerListing = (club: ClubManagerListingDefinition) => {
+const ClubManagerListing = (input: ClubManagerListingDefinition) => {
 
     return (
         <tr>
-            <td colSpan={3} className={"col-11"}><Link to="/settings/info">{club.title}</Link></td>
-            <td className={"col-1 text-center"}><Link to="/manageclubs/advancedsettings"><FontAwesomeIcon icon={faTrashAlt}/></Link></td>
+            <td colSpan={3} className={"col-11"}><Link to="/settings/info">{input.username}</Link></td>
+            <td className={"col-1 text-center"}><Button variant="outline-danger" onClick={ () => {input.setManagerUsername(input.username); input.confirmPopup(); }}><FontAwesomeIcon icon={faTrashAlt}/></Button></td>
         </tr>             
     )
 
